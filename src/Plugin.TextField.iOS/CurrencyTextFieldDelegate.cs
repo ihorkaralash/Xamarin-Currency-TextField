f7d706c40d19552c95447177e82ce8c779d80395
ihor.karalash <ikaralash@softjourn.com>
2020-06-26 12:14:13 +0300

    added custom locale support to text field delegate

--- src/Plugin.TextField.iOS/CurrencyTextFieldDelegate.cs
+++ src/Plugin.TextField.iOS/CurrencyTextFieldDelegate.cs
@@ -26,17 +26,26 @@ namespace Plugin.TextField.iOS
 
         public string CurrencySymbol
         {
+            get => CurrencyFormatter.CurrencySymbol;
             set => CurrencyFormatter.CurrencySymbol = value;
         }
 
         public string DecimalSeparator
         {
+            get => CurrencyFormatter.CurrencyDecimalSeparator;
             set => CurrencyFormatter.CurrencyDecimalSeparator = value;
         }
 
         public string GroupingSeparator
         {
+            get => CurrencyFormatter.CurrencyGroupingSeparator;
             set => CurrencyFormatter.CurrencyGroupingSeparator = value;
+        }
+
+        public NSLocale Locale
+        {
+            get => CurrencyFormatter.Locale;
+            set => CurrencyFormatter.Locale = value;
         }
 
         public FormatDelegate Format { get; set; }
